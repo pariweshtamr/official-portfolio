a709ff856abc397e9c04bc7a4c1d9bbc516b9aca
pariwesh <tamrpariwesh@gmail.com>
2023-02-08 10:40:13 +1100

    minor change in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,8 @@ const Footer = () => {
   return (
     <Container fluid className="footer">
       <div className="footer-content">
-        <p>&copy; 2023 Pariwesh Tamrakar</p>
+        <p>&copy; 2023 | Pariwesh Tamrakar</p>
+        <p>Design inspired by Nazar Miller</p>
       </div>
     </Container>
   )
