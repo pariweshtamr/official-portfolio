89ff52078f22504d4e840c03e9c8273991c2c602
pariwesh <tamrpariwesh@gmail.com>
2023-02-07 21:31:35 +1100

    deleted unused components

--- src/components/Button/Button.js
+++
@@ -1,9 +0,0 @@
-const Button = ({ value, styleClass, onClick }) => {
-  return (
-    <button className={`btn ${styleClass}`} onClick={(e) => onClick(e)}>
-      {value}
-    </button>
-  )
-}
-
-export default Button
